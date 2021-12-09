844178f90eb6fceca9ea65cfd0150206db1d6940
Richard Lin <elpato25@gmail.com>
2021-12-09 02:34:46 -0800

    Basic hardware interface

--- Smu/Smu.cpp
+++ Smu/Smu.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 
+#include "USBSerial.h"
+
 #define DEBUG_ENABLED
 #include "debug.h"
 
@@ -28,25 +30,73 @@ DmaSerial<1024> swdConsole(P0_8, NC, 115200);  // TODO increase size when have m
 //
 // Comms interfaces
 //
-// USB goes here
+USBSerial UsbSerial;
 
 //
-// Debugging defs
+// System
 //
-// SPI LcdSpi(P0_3, NC, P0_6);  // mosi, miso, sclk
-// DigitalOut LcdCs(P0_13);
-// DigitalOut LcdRs(P0_11);
-// DigitalOut LcdReset(P0_10);
+SPI SharedSpi(P0_3, P0_5, P0_6);  // mosi, miso, sclk
+
+DigitalOut DacLdac(P0_0);
+DigitalOut DacCurrNegCs(P0_1);
+DigitalOut DacCurrPosCs(P0_2);
+DigitalOut AdcVoltCs(P0_7);
+DigitalOut AdcCurrCs(P0_9);
+DigitalOut DacVoltCs(P0_18);
+
+DigitalOut EnableLow(P0_14);
+DigitalOut EnableHigh(P0_15);
+
+//
+// User interface
+//
+DigitalOut LcdCs(P0_13);
+DigitalOut LcdRs(P0_11);
+DigitalOut LcdReset(P0_10);
 
 DigitalOut LedR(P0_29), LedG(P0_28), LedB(P0_27);
 RgbActivityDigitalOut StatusLed(UsTimer, LedR, LedG, LedB, false);
 TimerTicker LedStatusTicker(1 * 1000 * 1000, UsTimer);
 
+DigitalIn SwitchL(P0_24, PinMode::PullUp);
+DigitalIn SwitchR(P0_25, PinMode::PullUp);
+DigitalIn SwitchC(P0_26, PinMode::PullUp);
+
 //
 // LCD and widgets
 //
-// St7735sGraphics<160, 80, 1, 26> Lcd(LcdSpi, LcdCs, LcdRs, LcdReset);
+St7735sGraphics<160, 80, 1, 26> Lcd(SharedSpi, LcdCs, LcdRs, LcdReset);
+TimerTicker LcdUpdateTicker(100 * 1000, UsTimer);
 
+const uint8_t kContrastActive = 255;
+const uint8_t kContrastStale = 191;
+
+const uint8_t kContrastBackground = 191;
+
+TextWidget widVersionData("USB PD SMU ", 0, Font5x7, kContrastActive);
+TextWidget widBuildData("  " __DATE__ " " __TIME__, 0, Font5x7, kContrastBackground);
+
+StaleNumericTextWidget widMeasV(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale);
+LabelFrameWidget widMeasVFrame(&widMeasV, "MEAS V", Font3x5, kContrastBackground);
+StaleNumericTextWidget widMeasI(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale);
+LabelFrameWidget widMeasIFrame(&widMeasI, "MEAS I", Font3x5, kContrastBackground);
+Widget* widMeasContents[] = {&widMeasVFrame, &widMeasIFrame};
+HGridWidget<2> widMeas(widMeasContents);
+
+StaleNumericTextWidget widSetV(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale);
+LabelFrameWidget widSetVFrame(&widSetV, "V", Font3x5, kContrastBackground);
+StaleNumericTextWidget widSetISrc(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale);
+LabelFrameWidget widSetISrcFrame(&widSetISrc, "I SRC", Font3x5, kContrastBackground);
+StaleNumericTextWidget widSetISnk(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale);
+LabelFrameWidget widSetISnkFrame(&widSetISnk, "I SNK", Font3x5, kContrastBackground);
+Widget* widSetContents[] = {&widSetVFrame, &widSetISrcFrame, &widSetISnkFrame};
+HGridWidget<3> widSet(widSetContents);
+
+TextWidget widBtns("U U U", 5, Font5x7, kContrastActive);
+LabelFrameWidget widBtnsFrame(&widBtns, "BTNS", Font3x5, kContrastBackground);
+
+Widget* widMainContents[] = {&widVersionData, &widBuildData, &widMeas, &widSet, &widBtnsFrame};
+VGridWidget<5> widMain(widMainContents);
 
 int main() {
   swdConsole.baud(115200);
@@ -60,7 +110,7 @@ int main() {
 
   UsTimer.start();
 
-  // Lcd.init();
+  Lcd.init();
 
 uint8_t i = 0;
   while (1) {
@@ -74,12 +124,30 @@ uint8_t i = 0;
         StatusLed.pulse(RgbActivity::kBlue);
       }
       i = (i + 1) % 3;
+
+      if (UsbSerial.configured()) {
+        UsbSerial.puts("hb");
+      }
     }
 
     StatusLed.update();
 
-    // if (LcdTicker.checkExpired()) {
-    //   Lcd.update();
-    // }
+    if (LcdUpdateTicker.checkExpired()) {
+      char btnsText[] = "U U U";
+      if (SwitchL == 0) {
+        btnsText[0] = 'D';
+      }
+      if (SwitchR == 0) {
+        btnsText[2] = 'D';
+      }
+      if (SwitchC == 0) {
+        btnsText[4] = 'D';
+      }
+      widBtns.setValue(btnsText);
+
+      widMain.layout();
+      widMain.draw(Lcd, 0, 0);
+      Lcd.update();
+    }
   }
 }
