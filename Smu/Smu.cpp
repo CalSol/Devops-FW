859a59785cb8c3f4f3341c4b6d8c7013d73be084
Richard Lin <elpato25@gmail.com>
2022-01-05 22:50:50 -0800

    wip calibration constants

--- Smu/Smu.cpp
+++ Smu/Smu.cpp
@@ -47,6 +47,10 @@ DigitalOut DacVoltCs(P0_18, 1);
 DigitalOut EnableHigh(P0_15);  // Current source transistor enable
 DigitalOut EnableLow(P0_14);  // Current sink transistor enable
 
+uint16_t kAdcCenter = 2042;  // Measured center value of the ADC
+uint16_t kDacCenter = 2048;  // Empirically derived center value of the DAC
+// uint16_t kDacCenter = 2048;
+
 //
 // User interface
 //
@@ -76,21 +80,48 @@ const uint8_t kContrastBackground = 191;
 TextWidget widVersionData("USB PD SMU ", 0, Font5x7, kContrastActive);
 TextWidget widBuildData("  " __DATE__ " " __TIME__, 0, Font5x7, kContrastBackground);
 
-StaleNumericTextWidget widMeasV(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 3);
-LabelFrameWidget widMeasVFrame(&widMeasV, "MEAS V", Font3x5, kContrastBackground);
-StaleNumericTextWidget widMeasI(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 3);
-LabelFrameWidget widMeasIFrame(&widMeasI, "MEAS I", Font3x5, kContrastBackground);
+StaleNumericTextWidget widMeasV(0, 2, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 2);
+TextWidget widAdcVSep(" ", 0, Font5x7, kContrastStale);
+NumericTextWidget widAdcV(0, 4, Font3x5, kContrastStale);
+Widget* widMeasVContents[] = {&widMeasV, &widAdcVSep, &widAdcV};
+HGridWidget<3> widMeasVGrid(widMeasVContents);
+LabelFrameWidget widMeasVFrame(&widMeasVGrid, "MEAS V", Font3x5, kContrastBackground);
+
+StaleNumericTextWidget widMeasI(0, 2, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 2);
+TextWidget widAdcISep(" ", 0, Font5x7, kContrastStale);
+NumericTextWidget widAdcI(0, 4, Font3x5, kContrastStale);
+Widget* widMeasIContents[] = {&widMeasI, &widAdcISep, &widAdcI};
+HGridWidget<3> widMeasIGrid(widMeasIContents);
+LabelFrameWidget widMeasIFrame(&widMeasIGrid, "MEAS I", Font3x5, kContrastBackground);
+
 Widget* widMeasContents[] = {&widMeasVFrame, &widMeasIFrame};
 HGridWidget<2> widMeas(widMeasContents);
 
-StaleNumericTextWidget widSetV(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 3);
-LabelFrameWidget widSetVFrame(&widSetV, "V", Font3x5, kContrastBackground);
-StaleNumericTextWidget widSetISrc(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 3);
-LabelFrameWidget widSetISrcFrame(&widSetISrc, "I SRC", Font3x5, kContrastBackground);
-StaleNumericTextWidget widSetISnk(0, 5, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 3);
-LabelFrameWidget widSetISnkFrame(&widSetISnk, "I SNK", Font3x5, kContrastBackground);
+
+StaleNumericTextWidget widSetV(0, 2, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 2);
+TextWidget widDacVSep(" ", 0, Font5x7, kContrastStale);
+NumericTextWidget widDacV(0, 4, Font3x5, kContrastStale);
+Widget* widSetVContents[] = {&widSetV, &widDacVSep, &widDacV};
+HGridWidget<3> widSetVGrid(widSetVContents);
+LabelFrameWidget widSetVFrame(&widSetVGrid, "V", Font3x5, kContrastBackground);
+
+StaleNumericTextWidget widSetISrc(0, 2, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 2);
+TextWidget widDacISrcSep(" ", 0, Font5x7, kContrastStale);
+NumericTextWidget widDacISrc(0, 4, Font3x5, kContrastStale);
+Widget* widSetISrcCntents[] = {&widSetISrc, &widDacISrcSep, &widDacISrc};
+HGridWidget<3> widSetISrcGrid(widSetISrcCntents);
+LabelFrameWidget widSetISrcFrame(&widSetISrcGrid, "I SRC", Font3x5, kContrastBackground);
+
+StaleNumericTextWidget widSetISnk(0, 2, 100 * 1000, Font5x7, kContrastActive, kContrastStale, Font3x5, 1000, 2);
+TextWidget widDacISnkSep(" ", 0, Font5x7, kContrastStale);
+NumericTextWidget widDacISnk(0, 4, Font3x5, kContrastStale);
+Widget* widSetISnkContents[] = {&widSetISnk, &widDacISnkSep, &widDacISnk};
+HGridWidget<3> widSetISnkGrid(widSetISnkContents);
+LabelFrameWidget widSetISnkFrame(&widSetISnkGrid, "I SNK", Font3x5, kContrastBackground);
+
 Widget* widSetContents[] = {&widSetVFrame, &widSetISrcFrame, &widSetISnkFrame};
 HGridWidget<3> widSet(widSetContents);
+
 
 TextWidget widBtns("U U U", 5, Font5x7, kContrastActive);
 LabelFrameWidget widBtnsFrame(&widBtns, "BTNS", Font3x5, kContrastBackground);
@@ -132,6 +163,8 @@ uint8_t i = 0;
     StatusLed.update();
 
     if (LcdUpdateTicker.checkExpired()) {
+      DacLdac = 1;
+
       SharedSpi.frequency(100000);
       SharedSpi.format(8, 0);
       AdcVoltCs = 0;
@@ -140,46 +173,55 @@ uint8_t i = 0;
       // first two clocks are for sampling, then one null bit, then data
       // and last bit (in a 16 bit transfer) is unused
       uint16_t adcv = (((uint16_t)(adcv0 & 0x1f) << 8) | adcv1) >> 1;
-      int32_t measMv = ((int64_t)adcv - 2048) * 3000 * kVoltRatio / 1000 / 4096;  // in mV
+      int32_t measMv = ((int64_t)adcv - kAdcCenter) * 3000 * kVoltRatio / 1000 / 4096;  // in mV
       AdcVoltCs = 1;
       
       AdcCurrCs = 0;
       uint8_t adci0 = SharedSpi.write(0);
       uint8_t adci1 = SharedSpi.write(0);
       uint16_t adci = (((uint16_t)(adci0 & 0x1f) << 8) | adci1) >> 1;
-      int32_t measMa = ((int64_t)adci - 2048) * 3000 * kAmpRatio / 1000 / 4096;  // in mA
+      int32_t measMa = ((int64_t)adci - kAdcCenter) * 3000 * kAmpRatio / 1000 / 4096;  // in mA
       AdcCurrCs = 1;
 
-      int32_t targetV = 420;  // mV
+      int32_t targetV = 2500;  // mV
       int32_t setVOffset = (int64_t)targetV * 4096 * 1000 / kVoltRatio / 3000;
-      uint16_t setV = 2048 - setVOffset;
+      uint16_t setV = kDacCenter - setVOffset;
       DacVoltCs = 0;
       SharedSpi.write(0x30 | ((setV >> 8) & 0x0f));
       SharedSpi.write(setV & 0xff);
       DacVoltCs = 1;
+      widSetV.setValue(targetV);
+      widDacV.setValue(setV);
 
       int32_t targetISrc = 200;  // mA
       int32_t setISrcOffset = (int64_t)targetISrc * 4096 * 1000 / kAmpRatio / 3000;
-      uint16_t setISrc = 2048 - setISrcOffset;
+      uint16_t setISrc = kDacCenter - setISrcOffset;
       DacCurrPosCs = 0;
       SharedSpi.write(0x30 | ((setISrc >> 8) & 0x0f));
       SharedSpi.write(setISrc & 0xff);
       DacCurrPosCs = 1;
+      widSetISrc.setValue(targetISrc);
+      widDacISrc.setValue(setISrc);
 
       int32_t targetISnk = -400;  // mA
       int32_t setISnkOffset = (int64_t)targetISnk * 4096 * 1000 / kAmpRatio / 3000;
-      uint16_t setISnk = 2048 - setISnkOffset;
+      uint16_t setISnk = kDacCenter - setISnkOffset;
       DacCurrNegCs = 0;
       SharedSpi.write(0x30 | ((setISnk >> 8) & 0x0f));
       SharedSpi.write(setISnk & 0xff);
       DacCurrNegCs = 1;
+      widSetISnk.setValue(targetISnk);
+      widDacISnk.setValue(setISnk);
 
       DacLdac = 0;
+
       EnableHigh = 1;
       EnableLow = 1;
 
       SharedSpi.frequency(10000000);
 
+      widAdcV.setValue(adcv);
+      widAdcI.setValue(adci);
       widMeasV.setValue(measMv);
       widMeasI.setValue(measMa);
       debugInfo("MeasV: %u => %li mV    MeasI: %u => %li mA    SetV: %u    SetISrc: %u    SetISnk %u", 
